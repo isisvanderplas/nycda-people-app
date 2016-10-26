786066547513a3e239937ca13627dab143a50c78
Isis van der Plas <isisvanderplas@hotmail.com>
2016-10-26 11:01:48 +0200

    finished project

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 
 var fs = require('fs');
 
-fs.readFile('people1.json', 'utf8',   (err, data)=> {
+fs.readFile('people1.json', 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  var people = (JSON.parse(data));
+  console.log(people);
+
+  fs.readFile('people2.json', 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
 
     var people = (JSON.parse(data));
     console.log(people);
+
+    fs.writeFile('peopleComplete.txt', people, (err, data) => {
+      if (err) {
+        throw err;
+      }
+       people = people.sort();
+       people = JSON.stringify(people);
+       console.log(people);
+    });
   });
-
-fs.readFile('people2.json', 'utf8',  (err, data)=> {
-    if (err) {
-      throw err;
-    }
-
-    var people = (JSON.parse(data));
-    console.log(people);
-  });
-
-
-
-fs.writeFile('peopleComplete.txt', people,  (err, data)=> {
-    if (err) {
-      throw err;
-    }
-     people = people.sort();
-     people = JSON.stringify(people);
-     console.log(people);
-  });
+});
